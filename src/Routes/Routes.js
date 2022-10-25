536bc366b59403a95878c9ddaed2ed2db81cecd6
Aminul <mdaminulislamsahin14@gmail.com>
2022-10-25 11:49:33 +0600

    hero section added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,3 +1,5 @@
+import Blog from "../components/Blog/Blog";
+
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Courses } = require("../components/Courses/Courses");
 const { default: Home } = require("../components/Home/Home");
@@ -15,6 +17,10 @@ const router = createBrowserRouter([
             {
                 path: 'courses',
                 element: <Courses></Courses>
+            },
+            {
+                path: 'blog',
+                element: <Blog></Blog>
             }
         ]
     }
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Hero.css'
+
+const Hero = () => {
+    return (
+      <div className="hero min-h-screen">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold hero-title">
+              Skill Development Platform
+            </h1>
+            <p className="mb-5">
+              Start streaming on-demand video lectures today from top level
+              instructors Attention heatmaps.
+            </p>
+            <Link to='courses'>
+              <button className="btn btn-primary">Get Started</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Hero;
